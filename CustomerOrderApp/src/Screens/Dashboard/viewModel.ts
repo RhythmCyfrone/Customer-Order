99fb8d47cd28ade6c89d4c9e2a9c5e233a86e6fe
RhythmCyfrone <rhythm.aggarwal@cyfrone.com>
2024-09-30 16:16:08 +0530

    updated search

--- CustomerOrderApp/src/Screens/Dashboard/viewModel.ts
+++ CustomerOrderApp/src/Screens/Dashboard/viewModel.ts
@@ -14,27 +14,28 @@ const useDashboardViewModel = () => {
     const [statusFlter, setStatusFilter] = useState<string>('All')
 
     useEffect(() => {
+        let tempList = []
         if(tableName === '') {
-            setDisplayTables([...tablesList].sort((a, b) => Number(a.id.slice(1)) - Number(b.id.slice(1))))
+            tempList = [...tablesList].sort((a, b) => Number(a.id.slice(1)) - Number(b.id.slice(1)))
         }else {
-            setDisplayTables(displayTables => displayTables.filter(table => table.id.toLowerCase().includes(tableName.toLowerCase())))
+            tempList = [...tablesList].filter(table => table.id.toLowerCase().includes(tableName.toLowerCase()))
         }
-    }, [tableName, tablesList])
 
-    useEffect(() => {
+        console.log(tempList)
+
         if(statusFlter === 'All') {
-            setDisplayTables([...tablesList].sort((a, b) => Number(a.id.slice(1)) - Number(b.id.slice(1))))
+            setDisplayTables(tempList.sort((a, b) => Number(a.id.slice(1)) - Number(b.id.slice(1))))
         }else if(statusFlter === 'Occupied') {
-            setDisplayTables(tablesList.filter(table => {
+            setDisplayTables(tempList.filter(table => {
                 return ['Occupied', 'Served', 'Billed'].includes(table.curr_status)
             }))
         }else if(statusFlter === 'Assigned') {
-            setDisplayTables(tablesList.filter(table => {
+            setDisplayTables(tempList.filter(table => {
                 return ['Reserved', 'Assigned'].includes(table.curr_status)
             }))
         }
-    }, [statusFlter])
-
+    }, [tableName, tablesList, statusFlter])
+    
     useEffect(() => {
         const updateTablesListData = async () => {
             const err = await getAllTables()
