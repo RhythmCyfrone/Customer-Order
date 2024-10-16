9f07ac1c7f0af0370be6940772a65a2afad1f750
RhythmCyfrone <rhythm.aggarwal@cyfrone.com>
2024-10-16 23:59:24 +0530

    Added GUID to each API

--- CustomerOrderApp/src/Services/HTTPServices/api.ts
+++ CustomerOrderApp/src/Services/HTTPServices/api.ts
@@ -1,9 +1,22 @@
 import axios, { AxiosInstance} from 'axios';
+import { Guid } from 'guid-typescript';
 
-const API_BASE_URL = import.meta.env.VITE_APP_COVIDB_BASE_URL_DEV
+const API_BASE_URL = import.meta.env.VITE_APP_COVIDB_BASE_URL_PROD
 export const apiClient: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+apiClient.interceptors.request.use((config) => {
+
+  const corrId = Guid.create().toString();
+
+  config.headers['x-cyfrone-corrid'] = corrId;
+
+  return config;
+
+}, (error) => {
+  return Promise.reject(error);
 });
