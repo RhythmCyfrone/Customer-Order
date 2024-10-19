98a0203710ab709ff956420026c5ba3282cf5d70
RhythmCyfrone <rhythm.aggarwal@cyfrone.com>
2024-10-19 22:07:17 +0530

    Updated free table sidebar

--- CustomerOrderApp/src/ViewModels/TableSelectedDashboardFreeViewModel.ts
+++ CustomerOrderApp/src/ViewModels/TableSelectedDashboardFreeViewModel.ts
@@ -1,10 +1,33 @@
 import { useAppSelector } from "../State/hooks";
+import { useState } from "react";
 
 const useTableSelectedDashboardFreeViewModel = () => {
     const currentTable = useAppSelector(state => state.tableSelect.selectedTable)
+    const [quickActions, setQuickActions] = useState(false)
+    const [newCustomerName, setNewCustomerName] = useState('')
+    const [newCustomerMobile, setNewCustomerMobile] = useState('')
+    const [countPersons, setCountPersons] = useState(0)
+    const [disableConfirmQuickActions, setDisableConfirmQuickActions] = useState(true)
+
+    const updateCountPersons = (count: number) => {
+        setCountPersons(count)
+    }
+
+    const handleNewCustomerNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewCustomerName(e.target.value)
+        if (e.target.value !== '') {
+            setDisableConfirmQuickActions(false)
+        }
+    }
+
+    const handleNewCustomerMobileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewCustomerMobile(e.target.value)
+    }
 
     return {
-        currentTable
+        currentTable, quickActions, setQuickActions,
+        newCustomerName, newCustomerMobile, handleNewCustomerNameChange, handleNewCustomerMobileChange,
+        countPersons, updateCountPersons, disableConfirmQuickActions
     }
 }
 
